15df62381acac6f06e2dab53e943ed8556fa640c
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-19 00:35:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,14 +3,14 @@ class Application
     resp = Rack::Response.new
 
     time =  Kernel.rand(1..24)
-    resp.write("The current time is: #{time}")
+    resp.write("The current time is: #{time}\n")
 
     if time.between?(12,18)
       resp.write("Good Afternoon!")
     elsif time.between?(19,24)
       resp.write("Good Night")
     else
-      resp.write("Good Moring!")
+      resp.write("Good Morning!")
     end
 
     resp.finish
